411f9420f236a36da4f9715561ad06effc8fc270
Cooper Veysey <cwveysey@gmail.com>
2022-06-17 16:21:04 -0400

    Style PaginationList MuiPaginationItem elements per design specifications.

--- client/src/PaginationList.js
+++ client/src/PaginationList.js
@@ -4,12 +4,41 @@ import Pagination from '@mui/material/Pagination';
 import { makeStyles } from '@material-ui/core/styles';
 const { stringify } = require('flatted');
 
-const useStyles = makeStyles(() => ({
-  ul: {
+const useStyles = makeStyles(theme => ({
+  paginationItemRoot: {
     "& .MuiPaginationItem-root": {
-      // color: "#fff"
+      "fontFamily": 'Source Serif Pro',
+      "fontWeight": '400',
+      /* Per https://thoughtbot.com/blog/how-to-make-quick-calculations-in-sketch,
+      "The calculation for the line height is: [font size]*[line-height]" - 
+       consequently, line height here is calculated via dividing the Sketch specified
+       line height (30) by the Sketch specified font size (24) */
+      "lineHeight": "1.25",
+      "fontSize": "24px",
+      "color": "rgb(54, 60, 60)",
+    },
+    "& .MuiPaginationItem-root.MuiPaginationItem-page.Mui-selected": {
+      "fontFamily": 'Source Serif Pro',
+      "color": "rgb(54, 60, 60)",
+      backgroundColor: "white",
+      "text-decoration": "inherit",
+      textDecoration: "underline",
+      "fontWeight": '700',
+      "text-underline-offset": "4.75px",
+      /* TODO Regarding the pagination items featured in the Sketch file,
+      note that while the selected item text features a 1px border, 
+      the underline associated with the selected item does not feature 
+      a border. Unable to quickly find a solution here, I moved on
+       to other tasks and will revisit this when time allows.
+
+       For reference, the following applies a border to the selected text
+       AND the underline (per the above and the Sketch file, we don't want
+       the underline to feature a border): 
+       "-webkit-text-stroke": "1px rgb(151, 151, 151)"
+      */
     }
   }
+
 }));
 
 export default function PaginationList(props) {
@@ -19,5 +48,5 @@ export default function PaginationList(props) {
     setSelectedPage(page);
     props.onPageSelection(page);
   };
-  return <Pagination classes={{ ul: classes.ul }} count={props.count} page={selectedPage} onChange={handleChange} />;
+  return <Pagination classes={{ root: classes.paginationItemRoot }} count={props.count} page={selectedPage} onChange={handleChange} />;
 }
